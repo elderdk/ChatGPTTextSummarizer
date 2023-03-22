0f95702b42a8924a4b00ebad78ed9cbb17bfdcfb
daniel <elderdk@DESKTOP-10UCPFB.localdomain>
2023-03-22 19:45:59 +0900

    fix typing error

--- ChatGPTTextSummarizer/pdf2text/pdf2text.py
+++ ChatGPTTextSummarizer/pdf2text/pdf2text.py
@@ -2,14 +2,14 @@ from PyPDF2 import PdfReader
 from pathlib import Path
 
 
-def get_text_from_pdf(fpath: Path) -> str:
+def get_text_from_pdf(fpath: str) -> str:
     """Get the text from a PDF file."""
-    
-    with open(fpath, 'rb') as f:
+
+    with open(fpath, "rb") as f:
         reader = PdfReader(f)
         pages = [page.extract_text() for page in reader.pages]
-        return '\n'.join(pages)
-    
+        return "\n".join(pages)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pass
