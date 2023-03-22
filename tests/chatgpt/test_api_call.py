c06125ebe4d7c7c413b2321550475b039c8deda6
daniel <elderdk@DESKTOP-10UCPFB.localdomain>
2023-03-23 01:44:29 +0900

    mock api call, remove unnecessary test

--- tests/chatgpt/test_api_call.py
+++ tests/chatgpt/test_api_call.py
@@ -1,11 +1,14 @@
 from ChatGPTTextSummarizer.chatgpt.api_call import APICaller
-import decouple
-from openai.error import AuthenticationError
+import pytest
+import openai
 
 
-def test_successful_api_caller():
+def test_successful_api_caller(mocker):
+    mock_response = {"choices": [{"text": "\n\nHello World!"}]}
+    mocker.patch("openai.Completion.create", return_value=mock_response)
+
     caller = APICaller(
-        api_key=decouple.config("OPENAI_API_KEY"),
+        api_key="asdasdasdasdasdasd",
         model="text-davinci-003",
         max_tokens=7,
         temperature=0,
@@ -14,21 +17,3 @@ def test_successful_api_caller():
 
     assert caller.get_text_result() == "\n\nHello World!"
     assert caller.count_tokens() == 4
-    assert caller.used_tokens == 9
-
-
-def test_error_raised_on_bad_api_key():
-    caller = APICaller(
-        api_key="aBadApiKey",
-        model="text-davinci-003",
-        max_tokens=7,
-        temperature=0,
-        prompt="Say hello world.",
-    )
-
-    try:
-        caller.get_text_result()
-    except AuthenticationError:
-        assert True
-    else:
-        assert False
